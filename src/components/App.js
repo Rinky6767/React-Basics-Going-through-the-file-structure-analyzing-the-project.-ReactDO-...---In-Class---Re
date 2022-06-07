0504f22ea6a5e419fd8b8b77de597859bea54d36
Rinky <71222541+Rinky6767@users.noreply.github.com>
2022-06-07 16:58:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main"></div>
+    <h1 data-ns-test="project-name"> Not thought yet</h1>
+    <h2 data-ns-tes ="project-description"> About</h2>
   )
 }
 
